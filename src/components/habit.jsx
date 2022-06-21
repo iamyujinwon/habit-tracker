d672ba4e8c4df1d2f0b1510b9cc016bceee8f5f5
Yujin Won <iamyujinwon@gmail.com>
2022-06-21 11:02:16 -0400

    add memo for habit add form component

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,6 +1,14 @@
 import { PureComponent } from "react";
 
 class Habit extends PureComponent {
+  componentDidMount() {
+    console.log(`habit: ${this.props.habit.name} mounted`);
+  }
+
+  componentWillUnmount() {
+    console.log(`habit: ${this.props.habit.name} will be unmounted`);
+  }
+
   handleIncrement = () => {
     this.props.onIncrement(this.props.habit);
   };
