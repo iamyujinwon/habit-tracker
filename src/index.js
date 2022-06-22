625098a6b57c26b337008c87ce4fc3cd9b3a7e4c
Yujin Won <iamyujinwon@gmail.com>
2022-06-22 14:41:11 -0400

    add App in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
-import SimpleHabit from './components/simpleHabit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SimpleHabit />
+    <App />
   </React.StrictMode>
 );
